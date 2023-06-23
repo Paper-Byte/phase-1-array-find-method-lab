98a46653661bfa973213ebc95cd43ad99822c039
Noah <noahhughesph@gmail.com>
2023-06-23 16:57:07 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+  const winningGame = record.find(findWin);
+  if (winningGame) {
+    return winningGame.year;
+  } else {
+    return undefined;
+  }
+}
+
+function findWin(element) {
+  if (element.result === 'W') {
+    return element.year;
+  }
+  return undefined;
+}
